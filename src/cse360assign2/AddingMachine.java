c248d7aa6ab2ce17f8ea4546b7e82abadf96b7f2
SirCactupus <abmasud@asu.edu>
2019-10-09 15:13:48 -0700

    Created cse360assign2 package, added AddingMachine.java, and added javadoc comments

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,62 @@
+package cse360assign2;
+
+/**
+ * Class creates an object that can add and subtract from a number.
+ */
+public class AddingMachine {
+
+	/**
+	 * int that keeps track of the total in the AddingMachine
+	 */
+	private int total;
+
+	/**
+	 * Constructor that has no parameters and creates an object with the total at 0 and initializes the history string;
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+
+	/**
+	 * Returns the value of the total.
+	 *
+	 * @return 0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+
+	/**
+	 * Adds a value to the total
+	 *
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+
+	/**
+	 * Subtracts a value to the total
+	 *
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+
+	/**
+	 * Returns the process through which the AddingMachine got its value.
+	 *
+	 * @return
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * Clears the value of the total
+	 */
+	public void clear() {
+	
+	}
+}
